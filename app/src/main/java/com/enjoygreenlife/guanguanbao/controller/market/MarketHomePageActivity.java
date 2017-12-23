ca645579f394a7801be208c4152df6b9148d1d76
Luther Tsai <Luther0429@gmail.com>
2017-12-23 15:51:15 +0800

    Update User Data in MarketHomePage

--- app/src/main/java/com/enjoygreenlife/guanguanbao/controller/market/MarketHomePageActivity.java
+++ app/src/main/java/com/enjoygreenlife/guanguanbao/controller/market/MarketHomePageActivity.java
@@ -5,14 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.enjoygreenlife.guanguanbao.R;
 import com.enjoygreenlife.guanguanbao.controller.market.drawCash.DrawCashMenuActivity;
+import com.enjoygreenlife.guanguanbao.model.ApiModel.ApiJsonFactory;
+import com.enjoygreenlife.guanguanbao.model.ApiModel.SharedFileHandler;
+import com.enjoygreenlife.guanguanbao.model.ApiModel.URLFactory;
+import com.enjoygreenlife.guanguanbao.model.DataModel.UserLoginResponse;
 import com.enjoygreenlife.guanguanbao.model.ViewModel.References.ActivityManager;
+import com.enjoygreenlife.guanguanbao.tool.httpConnectionTool.HttpConnectionTool;
+import com.enjoygreenlife.guanguanbao.tool.httpConnectionTool.HttpConnectionToolCallback;
+
+import java.util.Locale;
 
 public class MarketHomePageActivity extends AppCompatActivity {
 
     private LinearLayout _mDrawMoneyLayout;
+    private TextView _mUserName;
+    private TextView _mUserPoints;
+    private final ApiJsonFactory _apiJsonFactory = new ApiJsonFactory();
+    private SharedFileHandler _sharedFileHandler = new SharedFileHandler();
     private Class<?> mClss;
 
     @Override
@@ -20,9 +33,13 @@ public class MarketHomePageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_market_home_page);
         processView();
+        getUserData(_sharedFileHandler.retreiveUserSession(MarketHomePageActivity.this), _sharedFileHandler.retreiveUserID(MarketHomePageActivity.this));
     }
 
     private void processView() {
+        _mUserName = (TextView) findViewById(R.id.market_user_name);
+        _mUserPoints = (TextView) findViewById(R.id.market_user_points);
+
         _mDrawMoneyLayout = (LinearLayout) findViewById(R.id.layout_draw_money);
         _mDrawMoneyLayout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,5 +63,34 @@ public class MarketHomePageActivity extends AppCompatActivity {
         if (mClss.equals(DrawCashMenuActivity.class)) {
             startActivityForResult(intent, ActivityManager.MARKET_DRAW_CASH_ACTIVITY.getValue());
         }
+    }
+
+    private void getUserData(String session, String userID) {
+        String json = _apiJsonFactory.getUserInfoJson(session, userID);
+        // Call Connection Tool to process login
+        HttpConnectionTool httpConnectionTool = new HttpConnectionTool();
+        httpConnectionTool.postMethod(new URLFactory().getUerInfoURL(), json, new HttpConnectionToolCallback() {
+            @Override
+            public void onSuccess(String result) {
+                final UserLoginResponse userLoginResponse = _apiJsonFactory.parseUserLoginResponse(result);
+                System.out.println(result);
+                if (userLoginResponse.getCode() == 1) {
+
+                    // Update UI
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+//                          _progress.setVisibility(View.INVISIBLE);
+                            _mUserName.setText(userLoginResponse.getUser().getUserName());
+
+                            String totalPointsStr = String.format(Locale.getDefault(), "%.0f", userLoginResponse.getUser().getWallet()) + getResources().getString(R.string.unit_draw_cash_points);
+                            _mUserPoints.setText(totalPointsStr);
+                        }
+                    });
+                } else {
+                    finish();
+                }
+            }
+        });
     }
 }
